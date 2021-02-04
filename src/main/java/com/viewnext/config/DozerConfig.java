123c15d6e4f929a840f5953ab3cb946aa9394abf
fmask8 <jagalanf@gmail.com>
2021-02-04 12:35:18 +0100

    Probaaaando

---
+++ src/main/java/com/viewnext/config/DozerConfig.java
@@ -0,0 +1,24 @@
+package com.viewnext.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.dozer.DozerBeanMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DozerConfig {
+
+	@Bean
+    public DozerBeanMapper getMapper() {
+        
+		List<String> mappingFiles = Arrays.asList("dozer-configuration-mapping.xml");
+
+        DozerBeanMapper dozerBean = new DozerBeanMapper();
+        
+        dozerBean.setMappingFiles(mappingFiles);
+        
+        return dozerBean;
+    }
+}
