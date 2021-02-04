a607607e00b0a9d5f1c57a0c09b99d0aaee40ed7
tomlup <tomlup77@hotmail.com>
2021-02-04 11:57:04 +0100

    first commit

---
+++ src/main/java/com/viewnext/RegistrosMedicos/RegistrosMedicosApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.RegistrosMedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrosMedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrosMedicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/RegistrosMedicos/RegistrosMedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.RegistrosMedicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrosMedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
