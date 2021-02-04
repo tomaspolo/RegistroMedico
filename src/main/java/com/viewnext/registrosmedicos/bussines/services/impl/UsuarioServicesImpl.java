bd32db58e9111959e69051030fd3b167ef762549
fmask8 <jagalanf@gmail.com>
2021-02-04 15:12:47 +0100

    Incluidos servicios, implementaciones y repositories

---
+++ src/main/java/com/viewnext/registrosmedicos/bussines/services/impl/UsuarioServicesImpl.java
@@ -0,0 +1,27 @@
+package com.viewnext.registrosmedicos.bussines.services.impl;
+
+import java.util.List;
+
+import com.viewnext.registrosmedicos.bussines.model.Usuario;
+import com.viewnext.registrosmedicos.bussines.services.UsuarioServices;
+
+public class UsuarioServicesImpl implements UsuarioServices{
+
+	@Override
+	public List<Usuario> getAll() {
+		
+		return null;
+	}
+	
+	@Override
+	public Usuario read(String DNI) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		
+		return null;
+	}
+}
